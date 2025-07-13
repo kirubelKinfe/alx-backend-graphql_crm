0223ddb8bacd9b7f8b42a85d0fe3484556d78cb6
Kirubel Kinfe <kirubelk@act.com.et>
2025-07-13 21:30:33 +0300

    adjusting configurations

--- alx-backend-graphql_crm/settings.py
+++ alx-backend-graphql_crm/settings.py
@@ -137,9 +137,11 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
-CELERY_BEAT_SCHEDULE = {
-    'generate-crm-report': {
-        'task': 'crm.tasks.generate_crm_report',
-        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
-    },
-}
+# Remove or comment out CELERY_BEAT_SCHEDULE if not using celery
+# from celery.schedules import crontab
+# CELERY_BEAT_SCHEDULE = {
+#     'generate-crm-report': {
+#         'task': 'crm.tasks.generate_crm_report',
+#         'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+#     },
+# }
--- alx_backend_graphql_crm/settings.py
+++
@@ -1,6 +0,0 @@
-INSTALLED_APPS = [
-    'django_crontab',
-]
-CRONJOBS = [
-    ('0 */12 * * *', 'crm.cron.update_low_stock'),
-] 
